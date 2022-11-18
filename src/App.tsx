54020e25d9ebe0e96eb149eedeb15e59394b0422
Michelle Laurenti <asmeikal@me.com>
2022-11-18 19:01:10 +0100

    setup the best bingo ever

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { giorgione } from './giorgione';
+import styles from './App.module.css';
+
+function randomize(input: string[]): string[][] {
+  let available = [...input];
+  const result = [];
+  for (let i = 0; i < 4; i++) {
+    const row = [];
+    for (let j = 0; j < 4; j++) {
+      const idx = Math.floor(Math.random() * available.length);
+      row.push(available[idx]);
+      available.splice(idx, 1);
+    }
+    result.push(row);
+  }
+  return result;
+}
+
+const rows = randomize(giorgione);
+
+function Row({ elements }: { elements: string[] }) {
+  return (
+    <div className={styles.row}>
+      {elements.map(el => (
+        <div key={el}><div>{el}</div></div>
+      ))}
+    </div>
+  )
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>B come BINGO</h1>
+      <div className={styles.container}>
+        <Row elements={rows[0]} />
+        <Row elements={rows[1]} />
+        <Row elements={rows[2]} />
+        <Row elements={rows[3]} />
+        {/*<Row elements={rows[4]} />*/}
+      </div>
+    </>
   );
 }
 
